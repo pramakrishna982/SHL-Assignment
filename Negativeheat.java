18bf64214fb49c5ba2e6692a67e886501f467814
Ramakrishna <93516813+pramakrishna982@users.noreply.github.com>
2021-12-02 17:04:20 +0530

    Add files via upload

---
+++ Negativeheat.java
@@ -0,0 +1,17 @@
+package javafirst;
+
+public class Negativeheat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {3,-3,5,8,-9,4,-98};
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<0) {
+				System.out.println("Negative temp:"+ arr[i]);
+			}
+			
+		}
+
+	}
+
+}
